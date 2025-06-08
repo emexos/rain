10e03bec8ec796fc0a06616547a20f773b933b75
emexOS 14 <jonsons.hub@gmail.com>
2025-06-08 21:45:49 +0200

    #2

--- install.sh
+++ install.sh
@@ -10,33 +10,33 @@ confirm() {
   read -p "$1 [y/N] " yn
   case "$yn" in
     [Yy]*) return 0 ;;
-    *) echo "[x] Aborted"; exit 1 ;;
+    *) echo " [x] Aborted"; exit 1 ;;
   esac
 }
 
-echo "-=======================================-"
+echo " -- : Rain : --------------------------------------------------"
 echo " -> Install 'rain' to $TARGET"
-confirm "   Proceed with installation to $TARGET?"
-confirm "   Are you absolutely sure this is the correct target directory?"
+confirm "    Proceed with installation to $TARGET?"
+confirm "    Are you absolutely sure this is the correct target directory?"
 
 if [ -f "$TARGET" ]; then
-  confirm "  'rain' already exists at $TARGET. Overwrite?"
+  confirm "    'rain' already exists at $TARGET... Overwrite?"
 fi
 
 curl -fsSL "$RAW_BASE/rain" -o "$TMP_BIN"
 if [ ! -f "$TMP_BIN" ]; then
-  echo "-> Error: download failed."
+  echo " -> Error: download failed."
   exit 1
 fi
 
 echo "-----------------------"
 echo " -> chmod +x $TMP_BIN..."
 chmod +x "$TMP_BIN"
-echo "\n"
+echo " "
 
 echo "------------------------------------------"
 echo " -> move $TMP_BIN to $TARGET..."
 sudo mv "$TMP_BIN" "$TARGET"
-echo "\n"
+echo " "
 
 echo " -> Finish - start the programm with "rain""
